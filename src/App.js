074d3b701ba836261871a8e22d8e00e95743fa6f
Mizuki IKEDA <mizulicious@gmail.com>
2022-09-20 15:59:02 +0200

    essai dégradé tailwing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">Keep Up !!!</h1>
+      <h1 className="text-center text-9x1 font black bg-gradient-to-r from-blue-700 to-red-700 text-transparent bg-clip-text">Commence ici en bleu et finit en rouge !</h1>
     </div>
   );
 }
