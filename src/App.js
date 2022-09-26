98efe56dd44d9312c545637431d771cfd709bcb7
Mizuki IKEDA <mizulicious@gmail.com>
2022-09-26 10:04:04 +0200

    typescript fichier config

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1 className="text-center text-9x1 font black bg-gradient-to-r from-blue-700 to-red-700 text-transparent bg-clip-text">Commence ici en bleu et finit en rouge !</h1>
-    </div>
-  );
-}
-
-export default App;
