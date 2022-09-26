2730480a9233b3c86c57d651ebc5c08cc304fcf1
Mizuki IKEDA <mizulicious@gmail.com>
2022-09-26 13:00:40 +0200

    image inclus homepage

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font black bg-gradient-to-r from-blue-700 to-red-700 text-transparent bg-clip-text">Commence ici en bleu et finit en rouge !</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,6 +1,12 @@
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <div className="border-black">Keep'Up </div>
+        <div>
+            <img src="./img/illustration_suscribe.svg" />
+            <img src='./img/photo_acceuil.svg' />
+        </div> 
+        </div>
     );
 }
 
