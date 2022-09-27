a442d52589081b0bb3768a8472396e2ec06a2958
antoine <antoine.lefrancq.manul@gmail.com>
2022-09-27 03:20:28 +0200

    création d'un state local dans signup, les champs sont controllés par le state et la modificaiton des champs par le set... du useState

---
+++ src/Components/Globals/Input/Radio/index.tsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+interface Props{
+  name:string,
+  value:any;
+  id?:string;
+}
+
+const InputRadio:React.FC<Props> = ({name, value,id}) => {
+  console.log(value);
+  return (
+    <input className='textInput' type={name} value={value} id={id?id:undefined} />
+  ); 
+}; 
+
+InputRadio.propTypes ={
+  name:PropTypes.string.isRequired,
+  value:PropTypes.any.isRequired,
+  id:PropTypes.string,
+};
+export default InputRadio;
--- src/Components/Signup/index.tsx
+++ src/Components/Signup/index.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
 import Header from '../Globals/Header';
-import InputElement from '../Globals/Input';
+import InputRadio from '../Globals/Input/Radio';
+import InputText from '../Globals/Input/Text';
 
+//Typescript interface
+interface sportNode{
+  sport:string,
+  level:string
+}
+type sportArray = sportNode[];
+
+//Function component
 const Signup:React.FC = () => {
+//State local
+  const [name, setName] = useState<string>('');
+  const [email, setMail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [phoneNumber, setPhone] = useState<number|null>(null);
+  const [birthDate, setDate] = useState<Date>(new Date(11/11/1111));
+  const [gender, setGender] = useState<'male'|'female'|'non-binary'>('non-binary');
+  const [city, setCity] = useState<string>('');
+  const [zip, setZip] = useState<number|null>(null);
+  const [disability, setDisability] = useState<boolean>(false);
+  const [sports, setSports] = useState<sportArray>([{sport:'', level:''}]);
+  const [moreInformations, setInformations] = useState<string>('');
+
+  //Components
   return (
     <>
       <div>Signup</div>
@@ -10,35 +34,43 @@ const Signup:React.FC = () => {
                 C’est partie ! Commence d’abord par te créer un compte 
         </h1>
         <form className='flex flex-col items-end'>
-          <InputElement />
-          <InputElement />
-          <InputElement />
-          <InputElement />
+          <InputText value={name} name='text' placeholder='Nom complet' changeField={setName} />
+          <InputText value={email} name='email' placeholder='E-mail' changeField={setMail} />
+          <InputText value={password} name='password' placeholder='Mot de passe' changeField={setPassword} />
+          <InputText value={phoneNumber}  name='tel' placeholder='Numéro de mobile' changeField={setPhone} />
           <p className='signup-p signup-p-birth'>
             Date de naissance
           </p>
           <fieldset className='flex flex-row'>
-            <InputElement />
-            <InputElement />
-            <InputElement />
+            <InputText value={0} name='number' placeholder='jour' changeField={setDate} />
+            <InputText value={0} name='number' placeholder='mois' changeField={setDate} />
+            <InputText value={0} name='number' placeholder='année' changeField={setDate} />
           </fieldset>
           <fieldset className='flex flex-row'>
-            <label>
-              <InputElement />
+            <InputRadio value={'t'} name='radio' id='male' />
+            <label htmlFor='male'>
+              Homme
             </label>
-            <label>
-              <InputElement />
+            <InputRadio value={'f'} name='radio' id='female' />
+            <label htmlFor='female'>
+              femme
+            </label>
+            <InputRadio value={'f'} name='radio' id='non-binary' />
+            <label htmlFor='non-binary'>
+              non binaire
             </label>
           </fieldset>
-          <InputElement />
-          <InputElement />
+          <InputText value={city} name='text' placeholder='Ville' changeField={setCity}/>
+          <InputText value={zip} name='number' placeholder='Code postal' changeField={setZip}/>
           <p className='signup-p'>As-tu un ou plusieurs handicap(s) ?</p>
           <fieldset className='flex flex-row'>
-            <label>
-              <InputElement />
+            <InputRadio value='t' name='radio' id='yes' />
+            <label htmlFor='yes'>
+              oui
             </label>
-            <label>
-              <InputElement />
+            <InputRadio value='f' name='radio' id='no'/>
+            <label htmlFor='no'>
+              non
             </label>
           </fieldset>
           <p className='signup-p'>
