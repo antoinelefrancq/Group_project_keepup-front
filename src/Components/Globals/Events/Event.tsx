1197c68aa2d677ef23dab7cf7959bac0647bf8be
Mizuki IKEDA <mizulicious@gmail.com>
2022-09-28 09:30:05 +0200

    modif Events et Event

--- src/Components/Globals/Events/Event.tsx
+++ src/Components/Globals/Events/Event.tsx
@@ -1,9 +1,21 @@
 function Event() {
-    return (
-        <div className=" bg-white border-2 p-16 w-screen h-8 ">
-
+  return (
+    <div className="bg-white rounded-lg w-[340px] p-0.5 h-auto flex">
+      <div className="bg-blue rounded-l-lg flex">
+        <div className="flex flex-col align-center justify-center">
+          <div className="text-white border-b-2 w-full text-center">Date</div>
+          <div className="text-white text-center">Sport Logo</div>
         </div>
-    );
+        <div className="text-white text-center">Photo profil</div>
+      </div>
+      <div className="text-blue grow">
+        <h3>Tuesday - leg day !</h3>
+        <p className="text-sm">Keeper: Benoît</p>
+        <p className="text-[0.7rem]">Activité: musculation <span className="bg-blue rounded-md text-white text-[0.7rem]">Amateur</span></p>
+        <p><span className="bg-blue rounded-md text-white text-[0.8rem]">1/2 place disponible</span><span className="bg-blue rounded-md text-white text-[0.7rem]">16:00</span></p>
+      </div>
+    </div>
+  );
 }
 
 export default Event;
--- src/Components/Globals/Events/index.tsx
+++ src/Components/Globals/Events/index.tsx
@@ -1,12 +1,12 @@
-import Event from "./Event";
+import Event from './Event';
 
 function Events() {
-    return (
-        <div className="flex flex-col absolute">
-            <Event />
-            <Event />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-5 absolute -top-28">
+      <Event />
+      <Event />
+    </div>
+  );
 }
 
 export default Events;
