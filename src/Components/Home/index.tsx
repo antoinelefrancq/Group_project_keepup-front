ea58b57ee0cf98e83b382463bb828e2e384bbb39
rmncld <colard.romain@gmail.com>
2022-09-28 11:40:23 +0200

    création form login_mobile

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -4,7 +4,7 @@ import Events from '../Globals/Events';
 
 function Home() {
   return (
-    <div className="bg-[length:1300px_650px] bg-no-repeat bg-top bg-home w-screen h-screen relative">
+    <div className="bg-[length:1300px_650px] bg-no-repeat bg-top bg-home w-screen h-screen -m[-12px] relative">
       <section className="bg-opacity-20 bg-black text-white pb-6 mb-15">
         <h3 className="text-3xl pt-7 pb-1 pl-6 font-pangram tracking-wide"><p>Besoin de</p><p>Motivation ?</p></h3>
         <div className="pl-6 text-xl font-pangram">
--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -1,7 +1,34 @@
-function Login() {
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import InputText from '../Globals/Input/Text';
+
+
+//Function component
+const Login:React.FC = () => {
+//State local
+  const [email, setMail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  //Components
   return (
-    <div>Login</div>
+    <div className='login'>
+      <>
+      
+        <section className="flex flex-col h-80">
+          <h1 className='text-blueCustom signup-title flex flex-col items-center'>
+                Te Revoilà !
+          </h1>
+          <form className='flex flex-col items-center gap-5'>
+            <InputText value={email} name='email' placeholder='E-mail' changeField={setMail} />
+            <InputText value={password} name='password' placeholder='Mot de passe' changeField={setPassword} />
+            <Link to="/"><button type="button" className="rounded-full py-3 px-8 text-2xl bg-gradient-to-b from-red to-blue hover:from-pink-500 hover:to-yellow-500 text-white">Connexion</button></Link>
+
+          </form>
+        </section>
+     
+      </>
+    </div>
   );
-}
+};
 
 export default Login;
