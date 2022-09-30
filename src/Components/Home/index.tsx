b18cd839779b801d7068e14d0c781321f9a58606
rmncld <colard.romain@gmail.com>
2022-09-30 10:26:04 +0200

    modification event exemple

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -55,7 +55,7 @@ function Home() {
             </div>
             <div className="text-blue">
               <h3 className="px-1">Tuesday - leg day !</h3>
-              <p className="px-1 text-[15px]">Keeper: Benoît</p>
+              <p className="px-1 text-[15px]">Keeper: Mizuki</p>
               <p className="px-1 text-[13px]">Activité: musculation <span className="bg-blue rounded-full text-white text-[11px] p-1">Amateur</span></p>
               <p className="flex justify-start items-end gap-4 py-2 pr-3">
                 <span className="bg-blue rounded-r-full text-white text-[13px] p-2">1/2 place disponible</span>
@@ -64,7 +64,7 @@ function Home() {
             </div>
           </div>
           <div className="bg-white rounded-lg w-full p-0.5 h-auto flex">
-            <div className="bg-blue rounded-l-lg flex items-center">
+            <div className="bg-red rounded-l-lg flex items-center">
               <div className="flex flex-col align-center justify-center w-[66px]">
                 <div className="text-white border-b-2 w-full text-center text-[15px] font-bold"><p>Jeu</p><p>22</p></div>
                 <div className="flex justify-center">
@@ -83,11 +83,11 @@ function Home() {
             </div>
             <div className="text-blue">
               <h3 className="px-1">Tuesday - leg day !</h3>
-              <p className="px-1 text-[15px]">Keeper: Benoît</p>
-              <p className="px-1 text-[13px]">Activité: musculation <span className="bg-blue rounded-full text-white text-[11px] p-1">Amateur</span></p>
+              <p className="px-1 text-[15px]">Keeper: Nathan</p>
+              <p className="px-1 text-[13px]">Activité: marche rapide <span className="bg-orange rounded-full text-white text-[11px] p-1">Amateur</span></p>
               <p className="flex justify-start items-end gap-4 py-2 pr-3">
-                <span className="bg-blue rounded-r-full text-white text-[13px] p-2">1/2 place disponible</span>
-                <span className="bg-blue rounded-full text-white text-[11px] px-2 py-1">16:00</span>
+                <span className="bg-red rounded-r-full text-white text-[13px] p-2">1Complet</span>
+                <span className="bg-blue rounded-full text-white text-[11px] px-2 py-1">10:00</span>
               </p>
             </div>
           </div>
