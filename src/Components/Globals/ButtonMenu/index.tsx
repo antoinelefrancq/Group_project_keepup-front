e1710ccb77b65ae4886ab8b29875253a63128512
Rmn_Cld <colard.romain@gmail.com>
2022-10-03 15:41:55 +0200

    fixed button-group

--- src/Components/Globals/ButtonMenu/index.tsx
+++ src/Components/Globals/ButtonMenu/index.tsx
@@ -1,14 +1,14 @@
 function ButtonMenu() {
   return (
-    <div className="fixed bottom-10 left-1/2 -translate-x-1/2">
+    <div className="w-64 fixed bottom-10 left-1/2 -translate-x-1/2 ">
       <div className="flex flex-row relative">
-        <button onClick={() => console.log('bouton de gauche')} className=" flex justify-between bg-gradient-to-b from-red to-blue hover:from-pink-500 shadow shadow-black hover:to-yellow-500 bg-white text-gray-800 font-bold pr-6 rounded-l">
+        <button onClick={() => console.log('bouton de gauche')} className="flex justify-between bg-gradient-to-b from-red to-blue hover:from-pink-500 shadow shadow-black hover:to-yellow-500 bg-white text-gray-800 font-bold pr-6 rounded-l">
           <img src='./img/Loupe.svg' alt="logo_loupe" className="flex py-4 translate-x-6"/>
           <div onClick={(event) => {
             event.stopPropagation();
             console.log('bouton du millieu');
-          }} className="translate-x-14 p-1 rounded-full from-rose-400 via-fuchsia-500 to-indigo-500 bg-gradient-to-b">
-            <span className="block text-black px-3 py-4 border-none font-semibold rounded-full bg-white">
+          }} className="btn-block translate-x-14 p-1 rounded-full from-rose-400 via-fuchsia-500 to-indigo-500 bg-gradient-to-b">
+            <span className="block text-black px-3 py-4 border-none font-semibold rounded-full bg-white btn-block">
               <img src='./img/Logo_menu.svg' alt="logo_menu"/>
             </span>
           </div>
