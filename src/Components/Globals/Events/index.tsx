bb7c8faf04497064a57880ac6cb68066e4433e66
Mizuki IKEDA <mizulicious@gmail.com>
2022-10-05 10:40:24 +0200

    ajout bouton map

--- src/Components/Globals/Events/index.tsx
+++ src/Components/Globals/Events/index.tsx
@@ -4,13 +4,15 @@ import Event from './Event';
 
 function Events() {
   return (
-    <div className="flex flex-col gap-3 p-2 overflow-y-auto">
-      <Event />
-      <Event />
-      <Event />
-      <ButtonMenu />
-
-    </div>
+    <>
+      <img src="./img/Logo_Map.svg" alt="logo_map" className="fixed right-0 inset-y-1/2 py-5 px-2 rounded-l-lg shadow-md shadow-[#808080] bg-white"/>
+      <div className="flex flex-col gap-3 p-2 overflow-y-auto">
+        <Event />
+        <Event />
+        <Event />
+        <ButtonMenu />
+      </div>
+    </>
   );
 }
 
