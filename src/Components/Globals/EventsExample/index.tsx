54a87bd0eeaf38290fd719ae0579d006274c251d
Mizuki IKEDA <mizulicious@gmail.com>
2022-10-05 09:41:58 +0200

    EventsExample modif2

--- src/Components/Globals/EventsExample/index.tsx
+++ src/Components/Globals/EventsExample/index.tsx
@@ -26,7 +26,7 @@ function EventsExample() {
         </div>
       </div>
       <div className="bg-white rounded-lg w-full p-0.5 flex items-center">
-        <div className="bg-blue rounded-l-lg flex items-center pr-[6px] h-[110px]">
+        <div className="bg-red rounded-l-lg flex items-center pr-[6px] h-[110px]">
           <div className="flex flex-col align-center justify-center w-[66px] px-2">
             <div className="text-white border-b-2 w-full text-center text-[15px] font-bold pb-1">
               <p>Mar</p><p>20</p>
@@ -42,9 +42,9 @@ function EventsExample() {
         <div className="text-blue">
           <h3 className="px-1 text-[15px]">Course à pied !</h3>
           <p className="px-1 text-[15px]">Keeper: Benoît</p>
-          <p className="px-1 text-[13px]">Activité: musculation <span className="bg-blue rounded-full text-white text-[11px] p-1">Amateur</span></p>
+          <p className="px-1 text-[13px]">Activité: course à pied<span className="bg-orange rounded-full text-white text-[11px] p-1">Intermédiaire</span></p>
           <p className="flex justify-start items-end gap-4 py-2 pr-3">
-            <span className="bg-blue rounded-r-full text-white text-[13px] p-1">1/2 place disponible</span>
+            <span className="bg-red rounded-r-full text-white text-[13px] p-1">Complet</span>
             <span className="bg-blue rounded-full text-white text-[11px] px-2 py-0.5">16:00</span>
           </p>
         </div>
