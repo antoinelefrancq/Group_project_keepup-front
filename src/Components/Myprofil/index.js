c3668fd4f7115decf8cf3f38f0ffa98542510d0d
Tekdey <devw.nbardi@gmail.com>
2022-10-07 20:35:50 +0200

    fetch user data from asunc redux thunk

--- src/Components/Myprofil/index.js
+++ src/Components/Myprofil/index.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react';
 import * as api from '../../api/routes';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { useParams} from 'react-router-dom';
-import { importLocalData } from '../../redux/reducer/userReducer';
+import { useParams } from 'react-router-dom';
+import { getUserData, importLocalData } from '../../redux/reducer/userReducer';
 import { addSport, deleteSport } from '../../redux/reducer/formSignup';
 import { useAuth } from '../App/ProtectedRoute';
 
@@ -22,7 +22,6 @@ const INITIAL_STATE = {
   zipcode: '62200',
 };
 
-
 //Function Component
 const Profil = () => {
   //
@@ -34,9 +33,8 @@ const Profil = () => {
 
   const dispatch = useDispatch();
 
-
-  const [isModifyingUser,setIsModifyingUser]=useState(false);
-  const [isModifyingSports,setIsModifyingSports]=useState(false);
+  const [isModifyingUser, setIsModifyingUser] = useState(false);
+  const [isModifyingSports, setIsModifyingSports] = useState(false);
   const [form, setForm] = useState(INITIAL_STATE);
   const [select, setSelect] = useState({
     id: '',
@@ -44,16 +42,15 @@ const Profil = () => {
     level: '',
   });
   const [data, setData] = useState([]);
-  const {sportList, sportToSend} = useSelector((state) => state.form);
-  const [modalIsOpen,setModalIsOpen] = useState(false);
-  const {userData} = useSelector((state) => state.user);
+  const { sportList, sportToSend } = useSelector((state) => state.form);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const { userData } = useSelector((state) => state.user);
 
-  function dateFactorisation (dob){
-    const {date} = dob.split('_');
+  function dateFactorisation(dob) {
+    const { date } = dob.split('_');
     [date[0], date[2]] = [date[2], date[0]];
     return date.join('/');
   }
-
 
   // const {sportList} = useSelector((state) => state.form);
 
@@ -61,16 +58,16 @@ const Profil = () => {
    * Fetch profil data
    * Sport, Level, Gender
    */
-  const {userID} = useParams();
+  const { userID } = useParams();
   console.log('------> ici');
   console.log(userID);
 
-  useEffect(()=>{
-    api.getUserById(userID)
-      .then((response)=> console.log(response.data.user))
-      .catch((error) => console.log(`mon erreur => ${error}`));
-  },[]);
-  
+  /**
+   * Get user data by redux
+   */
+  useEffect(() => {
+    dispatch(getUserData(userID));
+  }, []);
 
   useEffect(() => {
     api
@@ -87,11 +84,9 @@ const Profil = () => {
       });
   }, []);
 
-
   useEffect(() => {
     setForm({ ...form, password: '', userData });
   }, [userData]);
-
 
   const handleChange = (event) => {
     const { target } = event;
@@ -115,21 +110,19 @@ const Profil = () => {
     setSelect((prevState) => ({ ...prevState, level: target.value }));
   };
 
-  const toggleModale = () =>{
+  const toggleModale = () => {
     setModalIsOpen(!modalIsOpen);
     console.log(modalIsOpen);
   };
 
-  const hideModale = () =>{
+  const hideModale = () => {
     setModalIsOpen(false);
     console.log(modalIsOpen);
   };
 
-
   const buttonUserEdit = () => {
     setIsModifyingUser(true);
   };
-
 
   const buttonUserValidator = () => {
     setIsModifyingUser(false);
@@ -142,131 +135,172 @@ const Profil = () => {
     <>
       <div className="md:flex md:flex-col md:justify-center md:items-center">
         <div className="anchor pb-2"></div>
-        <section onClick={()=>hideModale()} className="signup flex flex-col items-center pt-[9px] pb-10 px-[10%] md:flex-row bg-[#F2EFEB] relative">
-          <button className='absolute top-3 right-4'>
-            <img src="/img/bi_arrow-down-circle.svg" alt="flèche_du_bas"/>
+        <section
+          onClick={() => hideModale()}
+          className="signup flex flex-col items-center pt-[9px] pb-10 px-[10%] md:flex-row bg-[#F2EFEB] relative"
+        >
+          <button className="absolute top-3 right-4">
+            <img src="/img/bi_arrow-down-circle.svg" alt="flèche_du_bas" />
           </button>
           <div className="flex flex-col w-full justify-center items-center md:flex-row md:justify-around">
             <div className="md:w-1/2">
-              <div className="relative flex flex-col items-center">        
-                <h1 className="text-blueCustom text-xl pb-2">Mon profil</h1> 
+              <div className="relative flex flex-col items-center">
+                <h1 className="text-blueCustom text-xl pb-2">Mon profil</h1>
                 <button
                   onClick={(event) => {
                     event.stopPropagation();
                     toggleModale();
-                  }} 
-                  type='button'
-                  name='button'
-                  className='relative'
+                  }}
+                  type="button"
+                  name="button"
+                  className="relative"
                 >
                   <span className="absolute flex items-center justify-center h-5 w-5 top-1 right-1 bg-blueCustom rounded-full z-10">
-                    <img className='absolute w-1/2 z-20' src="/img/pencil.svg" alt="pencil"/>
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blueCustom opacity-75"/>
-                    <span className="relative inline-flex rounded-full h-5 w- bg-blueCustom"/>
+                    <img
+                      className="absolute w-1/2 z-20"
+                      src="/img/pencil.svg"
+                      alt="pencil"
+                    />
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blueCustom opacity-75" />
+                    <span className="relative inline-flex rounded-full h-5 w- bg-blueCustom" />
                   </span>
                   <img
                     className="w-20 h-20 rounded-full transition-all hover:brightness-75"
                     src="/img/Nathan_2.png"
                     alt="modifier mes coordonnées"
                   />
-                </button>  
-                <div className={modalIsOpen? 'absolute flex flex-col items-center bg-[#fff] -bottom-16 text-xs text-blueCustom rounded-lg border-blueCustom border-solid border-2':'hidden'} >
-                  <button><p className='p-2 w-40 border-b-2 border-b-blueCustom border-b-solid hover:text-pinkCustom hover:tracking-wider transition-all'>Changer mon avatar</p></button>
-                  <button onClick={()=>buttonUserEdit()}><p className='p-2 w-40  hover:text-pinkCustom hover:tracking-wider transition-all'>Éditer mon profil</p></button>
+                </button>
+                <div
+                  className={
+                    modalIsOpen
+                      ? 'absolute flex flex-col items-center bg-[#fff] -bottom-16 text-xs text-blueCustom rounded-lg border-blueCustom border-solid border-2'
+                      : 'hidden'
+                  }
+                >
+                  <button>
+                    <p className="p-2 w-40 border-b-2 border-b-blueCustom border-b-solid hover:text-pinkCustom hover:tracking-wider transition-all">
+                      Changer mon avatar
+                    </p>
+                  </button>
+                  <button onClick={() => buttonUserEdit()}>
+                    <p className="p-2 w-40  hover:text-pinkCustom hover:tracking-wider transition-all">
+                      Éditer mon profil
+                    </p>
+                  </button>
                 </div>
               </div>
             </div>
-            { !isModifyingUser &&
-                <div className='w-full'>
-                  <p className="text-center text-blueCustom pb-8">{userData.firstname} {userData.lastname}</p>
-                  <p className='infos rounded-sm text-blueCustom'>{userData.email}</p>
-                  <p className='infos rounded-sm text-blueCustom'>{userData.dob}</p>
-                  <p className='infos rounded-sm text-blueCustom'>{userData.gender}</p>
-                  <p className='infos rounded-sm text-blueCustom h-32'>{userData.description}</p>
-                  <p className='infos rounded-sm text-blueCustom'>{userData.city}</p>
-                  <p className='infos rounded-sm text-blueCustom'>{userData.zipcode}</p>
-                </div>
-            }
+            {!isModifyingUser && (
+              <div className="w-full">
+                <p className="text-center text-blueCustom pb-8">
+                  {userData.firstname} {userData.lastname}
+                </p>
+                <p className="infos rounded-sm text-blueCustom">
+                  {userData.email}
+                </p>
+                <p className="infos rounded-sm text-blueCustom">
+                  {userData.dob}
+                </p>
+                <p className="infos rounded-sm text-blueCustom">
+                  {userData.gender}
+                </p>
+                <p className="infos rounded-sm text-blueCustom h-32">
+                  {userData.description}
+                </p>
+                <p className="infos rounded-sm text-blueCustom">
+                  {userData.city}
+                </p>
+                <p className="infos rounded-sm text-blueCustom">
+                  {userData.zipcode}
+                </p>
+              </div>
+            )}
             {/** le form s'affiche uniquement si l'on a cliqué sur l'image pour modifier les informations du profil.*/}
-            {isModifyingUser && <form className="text-blueCustom w-full text-right pt-2 flex flex-col items-center">
-              <input 
-                onChange={handleChange}
-                value={form.firstname}
-                name="firstname"
-                type="text"
-                placeholder="Prenom" 
-                className="infos rounded-sm w-60 md:w-4/5"/>
-              <input 
-                onChange={handleChange}
-                value={form.lastname}
-
-                name="lastname" 
-                type="text"
-                placeholder="Nom"
-                className="infos rounded-sm w-60 md:w-4/5"/>
-              <input
-                onChange={handleChange}
-                value={form.email}
-                name="email"
-                type="text"
-                placeholder="email"
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <input
-                onChange={handleChange}
-                name='password'
-                type="password"
-                placeholder='mot de passe'
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <input
-                onChange={handleChange}
-                value={form.dob}
-                name='dob'
-                placeholder="date"
-                type="date"
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <input
-                onChange={handleChange}
-                value={form.gender}
-                placeholder='genre'
-                name="gender"
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <textarea
-                onChange={handleChange}
-                value={form.description}
-                placeholder='description'
-                name='description'
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <input
-                onChange={handleChange}
-                value={form.city}
-                type="text"
-                name='city'
-                placeholder='ville'
-                className="infos rounded-sm w-60 md:w-4/5"
-              />
-              <input
-                onChange={handleChange}
-                value={form.zipcode}
-                type="number"
-                name="zipcode"
-                className="infos rounded-sm w-60 md:w-4/5"
-                placeholder='code postal'
-              />
-            </form>
-            }
+            {isModifyingUser && (
+              <form className="text-blueCustom w-full text-right pt-2 flex flex-col items-center">
+                <input
+                  onChange={handleChange}
+                  value={form.firstname}
+                  name="firstname"
+                  type="text"
+                  placeholder="Prenom"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.lastname}
+                  name="lastname"
+                  type="text"
+                  placeholder="Nom"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.email}
+                  name="email"
+                  type="text"
+                  placeholder="email"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  name="password"
+                  type="password"
+                  placeholder="mot de passe"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.dob}
+                  name="dob"
+                  placeholder="date"
+                  type="date"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.gender}
+                  placeholder="genre"
+                  name="gender"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <textarea
+                  onChange={handleChange}
+                  value={form.description}
+                  placeholder="description"
+                  name="description"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.city}
+                  type="text"
+                  name="city"
+                  placeholder="ville"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                />
+                <input
+                  onChange={handleChange}
+                  value={form.zipcode}
+                  type="number"
+                  name="zipcode"
+                  className="infos rounded-sm w-60 md:w-4/5"
+                  placeholder="code postal"
+                />
+              </form>
+            )}
           </div>
           <div className="flex flex-col justify-center items-center">
-            <p className="text-center w-full pb-6 pt-6 text-blueCustom">Sport pratiqués :</p>
-            <button 
+            <p className="text-center w-full pb-6 pt-6 text-blueCustom">
+              Sport pratiqués :
+            </p>
+            <button
               onClick={(event) => console.log(event.target)}
-              className="bg-blueCustom text-white rounded-lg p-2">Voulez vous modifier la liste ?</button>
-            <div className="p-2 border-t-2 border-[#E3E3E3] w-full">
-            </div>
+              className="bg-blueCustom text-white rounded-lg p-2"
+            >
+              Voulez vous modifier la liste ?
+            </button>
+            <div className="p-2 border-t-2 border-[#E3E3E3] w-full"></div>
             {sportList.map((item, key) => {
               return (
                 <ul key={key}>
@@ -336,32 +370,29 @@ const Profil = () => {
                 </button>
               </div>
             </div>
-            <article className="relative bg-blueCustom rounded-lg flex flex-col items-center justify-center w-[77px] h-[108px] shadow-inner shadow-whiteCustom hover:shadow-[0_0_25px_rgba(10,106,191,1)] transition-all">  
+            <article className="relative bg-blueCustom rounded-lg flex flex-col items-center justify-center w-[77px] h-[108px] shadow-inner shadow-whiteCustom hover:shadow-[0_0_25px_rgba(10,106,191,1)] transition-all">
               <button
                 type="button"
                 className="flex border-[1.8px] justify-center items-center border-pinkCustom w-5 h-5 leading-5 rotate-45 absolute -top-1 -right-1 bg-whiteCustom rounded-full"
               >
                 <img src="/img/Vector_red.svg" alt="+" />
-              </button> 
+              </button>
               <div className="text-white border-b-2 m-1 text-center text-[15px] font-bold pb-2">
                 <p>Amateur</p>
               </div>
               <div className="flex justify-center pt-2">
-                <img
-                  src="/img/Beginner.svg"
-                  alt="logo_beginner"
-                />
+                <img src="/img/Beginner.svg" alt="logo_beginner" />
               </div>
             </article>
           </div>
-          { isModifyingUser &&
-                <button
-                  onClick={() => buttonUserValidator()}
-                  className="bg-blueCustom text-white rounded-lg p-2 md:mt-20"
-                >
-                  Appliquer les modifications
-                </button>
-          }
+          {isModifyingUser && (
+            <button
+              onClick={() => buttonUserValidator()}
+              className="bg-blueCustom text-white rounded-lg p-2 md:mt-20"
+            >
+              Appliquer les modifications
+            </button>
+          )}
         </section>
       </div>
     </>
