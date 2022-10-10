56f213dbb8e0b5240fd570168bbc881cd04e30a3
Mizuki IKEDA <mizulicious@gmail.com>
2022-10-10 10:04:28 +0200

    reajout lien About

--- src/Components/Globals/Header/index.js
+++ src/Components/Globals/Header/index.js
@@ -28,7 +28,8 @@ function Header() {
        */}
       {
         connected && 
-        <div className="flex flex-col font-bold text-blue text-[14px] pb-[28px] pr-10 sm:flex-row sm:text-[24px] sm:gap-5 ">
+        <div className="flex flex-col items-center font-bold text-blue text-[14px] pb-[28px] pr-10 sm:flex-row sm:text-[24px] sm:gap-5 ">
+          <Link to="/team"><button className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>About</span></button></Link>
           <button onClick={handleClick} className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>Déconnexion</span></button>
         </div>
       }
@@ -36,10 +37,10 @@ function Header() {
        * Connection
        */}
       {!connected &&        
-        <div className="flex flex-col font-bold text-blue text-[14px] pb-[28px] pr-10 sm:flex-row sm:text-[24px] sm:gap-5 ">
+        <div className="flex flex-col items-center font-bold text-blue text-[14px] pb-[28px] pr-10 sm:flex-row sm:text-[24px] sm:gap-5 ">
+          <Link to="/team"><button className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>About</span></button></Link>
           <Link to="/signup"><button className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>Inscription</span></button></Link>
           <Link to="/login"><button className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>Connexion</span></button></Link>
-          <Link to="/team"><button className='group block hover:text-red focus:outline-none'><span className='group-focus:text-red group-focus:underline'>Team</span></button></Link>
         </div>}
     </div>
   );
