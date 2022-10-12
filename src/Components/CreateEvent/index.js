842236c7190102544b17191e55cc9552675eaa73
Tekdey <devw.nbardi@gmail.com>
2022-10-12 14:14:51 +0200

    add: form create event

--- src/Components/CreateEvent/index.js
+++ src/Components/CreateEvent/index.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import * as api from '../../api/routes';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 
 //================== Fonction Composant
 function CreateEvent() {
-
   //================== State local
   const [select, setSelect] = useState({
     id: '',
     sport: '',
     level: '',
     gender: '',
+    city: '',
+    address: '',
+    date: '',
+
+    session: {
+      start: '',
+      end: '',
+    },
+    max: '',
   });
-  const {
-    register,
-    handleSubmit,
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   const [sports, setSports] = useState([]);
   const [levels, setLevels] = useState([]);
@@ -25,23 +30,25 @@ function CreateEvent() {
 
   //================== Appel API
   useEffect(() => {
-    api.fetchSportsLevel().then((response) => {
-      if(response.status === 200) {
-        const result = response.data;
-        setSports(result.sports);
-        setLevels(result.level);
-        setGenders(result.gender);
-      }
-    }).catch((error) => {
-      console.log('_______________');
-      console.log(error);
-      console.log('_______________');
-    });
+    api
+      .fetchSportsLevel()
+      .then((response) => {
+        if (response.status === 200) {
+          const result = response.data;
+          setSports(result.sports);
+          setLevels(result.level);
+          setGenders(result.gender);
+        }
+      })
+      .catch((error) => {
+        console.log('_______________');
+        console.log(error);
+        console.log('_______________');
+      });
   }, []);
-  
 
-  //================== Functions de modifications 
-  
+  //================== Functions de modifications
+
   /**
    *  Date factorization
    * @param {Date} date Form date (dob)
@@ -52,7 +59,7 @@ function CreateEvent() {
   //   const timestampSeconds = Math.floor(date.getTime() / 1000);
   //   return String(timestampSeconds);
   // }
-  
+
   const handleChangeSelectSport = (event) => {
     const { target } = event;
     const selectedIndex = target.options.selectedIndex;
@@ -75,49 +82,65 @@ function CreateEvent() {
   };
 
   const onSubmit = async (data) => {
+    console.log(data);
     // const dateChosen = dateFactorisation(data.date);
-    const dateChosen = data.date;
 
-    await api
-      .postEvent({
-        ...data,
-        date: dateChosen,
-        sport: select.id,
-        level: select.level,
-        gender: select.gender,
-        max: Number(data.max),
-        zipcode: Number(data.zipcode),
-        admin: '6331760ae98d6c76841f590e',
-        country:'France',
-        location:{
-          type:'Point',
-          'coordinates' : [45.763217322939916, 4.835893475715351]
-        }
-      })
-      .then((response) => {
-        console.log(response);
-        toast.success('Votre évenement a été créé');
-      })
-      .catch((error) => {
-        console.dir(error);
-        toast.error(error.response.data.error);
-      });
+    const newObj = {
+      ...data,
+      location: {
+        type: 'Point',
+        coordinates: [],
+      },
+    };
+    const address =
+      newObj.address + '%20' + newObj.city + '%20' + newObj.zipcode;
+    const url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json`;
+
+    const response = await axios.get(url);
+
+    if (response.lenght < 0 || response.status !== 200) {
+      console.log('rien trouvé');
+      return;
+    }
+
+    // const [] = response.data[0].
+    console.log('***********');
+    console.log(response.data[0]);
+
+    console.log('***********');
+
+    if (response.status === 200) {
+      newObj.location.coordinates = [
+        response.data[0].lat,
+        response.data[0].lon,
+      ];
+    }
+    console.log(newObj);
+    // await api
+    //   .postEvent(newObj)
+    //   .then((response) => {
+    //     console.log(response);
+    //     toast.success('Votre évenement a été créé');
+    //   })
+    //   .catch((error) => {
+    //     console.dir(error);
+    //     toast.error(error.response.data.error);
+    //   });
   };
-  
+
   //================== Components
   return (
     <>
       <div className="h-full flex flex-col justify-end items-center">
         <section className="session flex flex-col gap-5 justify-center items-center bg-[#F2EFEB] md:w-1/2">
-          <div className="text-blueCustom">
-            Créer une session
-          </div>
-          <form 
+          <div className="text-blueCustom">Créer une session</div>
+          <form
             onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col items-center gap-2 w-full text-right px-10">
+            className="flex flex-col items-center gap-2 w-full text-right px-10"
+          >
             <input
               placeholder="Titre"
-              {...register('name')}
+              {...register('title')}
               className="text-right px-1 w-full"
             />
             <textarea
@@ -128,6 +151,7 @@ function CreateEvent() {
             <select
               className="bg-[#ffffff] text-greyPlaceholder p-1 w-full"
               onChange={handleChangeSelectSport}
+              {...register('sport')}
             >
               <option disabled={true}>🏈🏀⚽🏓🏐</option>
               {sports?.map((sport) => {
@@ -136,7 +160,6 @@ function CreateEvent() {
                     key={sport._id}
                     value={sport.sport}
                     data-key={sport._id}
-            
                   >
                     {sport.sport}
                   </option>
@@ -144,6 +167,7 @@ function CreateEvent() {
               })}
             </select>
             <select
+              {...register('level')}
               onChange={handleChangeSelectLevel}
               className="bg-[#ffffff] text-greyPlaceholder p-1 w-full"
             >
@@ -155,12 +179,13 @@ function CreateEvent() {
               ))}
             </select>
             <select
+              {...register('gender')}
               onChange={handleChangeSelectGender}
               className="bg-[#ffffff] text-greyPlaceholder p-1 w-full"
             >
               <option disabled={true}>👩👨👽👾</option>
               {genders?.map((gender) => (
-                <option key={gender} value={gender} >
+                <option key={gender} value={gender}>
                   {gender}
                 </option>
               ))}
@@ -207,7 +232,10 @@ function CreateEvent() {
               type="text"
               className="text-right px-1 w-full"
             />
-            <button type="submit" className="bg-blue text-white mt-16 mb-5 py-3 w-20 rounded-lg shadow-md shadow-[#808080] flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue text-white mt-16 mb-5 py-3 w-20 rounded-lg shadow-md shadow-[#808080] flex justify-center"
+            >
               <img src="./img/Arrow_right.svg" alt="Arrow_right" className="" />
             </button>
           </form>
@@ -216,5 +244,5 @@ function CreateEvent() {
     </>
   );
 }
-  
+
 export default CreateEvent;
