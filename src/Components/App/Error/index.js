250d4a9e7ff3ee48aa75e77115831acc035983ca
antoine <antoine.lefrancq.manul@gmail.com>
2022-10-14 17:52:40 +0200

    proposition de 404 mobile terminée

--- src/Components/App/Error/index.js
+++ src/Components/App/Error/index.js
@@ -1,14 +1,18 @@
-
+import './style.scss';
 
 const ErrorPage = () =>{
-
   return (
     <div className="relative h-[calc(100vh-130px)] w-full">
       <img src='/img/error.png' className="absolute bottom-0 w-full mx-auto"/>
       <div className="text-white text-3xl md:text-5xl p-10 text-center">
         <p className="">Oups</p>
-        <p className="">il n'y a rien à voir ici</p>
-        <p className="mt-5 tracking-wide">ERREUR 404</p>
+        <p className="mb-5">il n'y a rien à voir ici</p>
+        <div className='hello text-red'>ERREUR</div>
+        <div className='hello text-orange'>ERREUR </div>
+        <div className='hello text-blue'>ERREUR </div>
+        <div className='text-red hello-more'> 404</div>
+        <div className='text-orange hello-more'> 404</div>
+        <div className='text-blue hello-more'> 404</div>
       </div>
     </div>
   );
